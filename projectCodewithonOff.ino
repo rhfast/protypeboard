37e190802d506343c4e1cc3786ab21ed40d30954
nrpowell0 <73355820+nrpowell0@users.noreply.github.com>
2020-10-24 13:11:02 -0500

    Add files via upload

--- projectCodewithonOff.ino
+++ projectCodewithonOff.ino
@@ -14,7 +14,10 @@ bool isOn = 0;      //0 = off, 1 = on, buzzes
 #define A_Z = 5
 float analogX = 0; int analogY = 0; int analogZ = 0;
 float volX = 0.0; float volY = 0.0; float volZ = 0.0;  
-float g_x = 0.0; float g_y = 0.0;float g_z = 0.0;  
+float g_x = 0.0; float g_y = 0.0;float g_z = 0.0; 
+
+//averages used for calibrating while no buzz
+float avgX = 0; float avgY; float avgZ;
 
 void setup() {
   pinMode(touchy, INPUT);
@@ -29,6 +32,9 @@ void setup() {
 }
 
 void loop() {
+  if (digitalRead(white) == HIGH){
+    calibrate();
+  }
   onOff();
   readMeter();
   //buzzModule();
@@ -79,9 +85,9 @@ void getGValues(){
    g = volts * (g/volts)
    dividing by 1.5 g results in something unitless, but seems to fit with the physics
   was previously 1.65, 1.65, 2.45 as ambient values that were subtracted out*/
-  g_x = (volX - 1.67) / 1.5 / 0.8;
-  g_y = (volY - 1.78) / 1.5/ 0.8;
-  g_z = (volZ - 2.19) / 1.5 / 0.8;
+  g_x = (volX - avgX) / 1.5 / 0.8;
+  g_y = (volY - avgY) / 1.5/ 0.8;
+  g_z = (volZ - avgZ) / 1.5 / 0.8;
    
   /*Serial.print("X: ");  Serial.print(g_x); Serial.print("\t");
   Serial.print("Y: ");  Serial.print(g_y); Serial.print("\t");
@@ -100,4 +106,19 @@ void printGValues() {
   Serial.print(",");
   Serial.print(g_z);
   Serial.print(",");Serial.println();
+}
+
+void calibrate(){
+  float sumX = 0;
+  float sumY = 0;
+  float sumZ = 0;
+  for (int i = 0; i < 1000; i++){
+    readMeter();
+    sumX = sumX + volX;
+    sumY = sumY + volY;
+    sumZ = sumZ + volZ;
+  }
+  avgX = sumX / 1000;
+  avgY = sumY / 1000;
+  avgZ = sumZ / 1000;
 }
